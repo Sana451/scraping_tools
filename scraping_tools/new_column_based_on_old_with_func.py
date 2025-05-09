f4206400a3e6ba8ab9d1eb18d549be8d5d1c130c
Sana451 <sana451@mail.ru>
2025-05-09 19:21:41 +0300

    Qwerty

--- scraping_tools/new_column_based_on_old_with_func.py
+++ scraping_tools/new_column_based_on_old_with_func.py
@@ -1,10 +1,13 @@
 import re
+from scraping_tools.html_clean_tools import del_attrs_from_scrapy_selector
 
 import pandas as pd
 from scrapy import Selector
 
 # Чтение CSV файла в DataFrame
-df = pd.read_csv('/home/sana451/PycharmProjects/scrapy_parsers/schmersal_com/schmersal_com/results/schmersal_com_.de.csv')
+df = pd.read_csv(
+    '/home/sana451/PycharmProjects/scrapy_parsers/de_rubix_com/de_rubix_com/results/de_rubix_com..de.3.csv'
+)
 
 
 def my_function(x):
@@ -14,35 +17,48 @@ def my_function(x):
 
     try:
         # Инициализация селектора для анализа HTML
-        sel = Selector(text=x)
+        sel = Selector(text=x, type="html")
 
-        # Вставьте вашу логику извлечения данных здесь
-        # Например, если нужно извлечь какое-то значение:
-        # result = sel.xpath("(//*[contains(text(), 'eCl@ss Nummer,')])[1]//text()").get(default="").strip()
-        result = sel.xpath("(//*[contains(text(), 'eCl@ss Nummer,')])[1]//following-sibling::*//p//text()"
-                           ).get(default="").strip()
+        root = sel.root
 
-        return result
+        res = []
+
+        for node in root.iter():
+            if node.tag == "b":
+                print(node.xpath("./text()"))
+                print(type(node.xpath("./text()")))
+                # res.append(node.xpath("./text()").get(default="").strip())
+            if  node.tag == "br":
+                res.append("\n")
+            # if node.text and node.text.strip():
+            #     res.append((node.text.strip(), node.tag))  # текст внутри тега
+
+            # if node.tail and node.tail.strip():
+            #     parent = node.getparent()
+            #     if parent is not None:
+            #         res.append((node.tail.strip(), parent.tag))  # текст между тегами принадлежит родителю
+            #     else:
+            #         res.append((node.tail.strip(), None))  # если родителя нет (редко)
+
+        # val = sel.xpath("//tr[contains(., 'Verp.-einheit')]//td[2]//text()").get(default="").strip()
+        val = "".join(res)
+
+
+        return val
     except Exception as e:
         # Обработка исключений, если что-то пошло не так
         print(f"Ошибка при обработке: {e}")
         return ""  # Возвращаем пустое значение в случае ошибки
-# def my_function(x):
-#     # Инициализация селектора для анализа HTML
-#     sel = Selector(text=x)
-#
-#     #
-#
-#     # eclass_value = response.xpath(
-#     #     "(//*[contains(text(), 'eCl@ss Nummer,')])[1]//following-sibling::*//p//text()").get(default="").strip()
-#     # result["eClass value"] = eclass_value
-#     result = "qwery"
-#     return result
 
+
+# my_function(x)
 # Применяем функцию к колонке 'A' и создаем новую колонку 'B'
-df['eClass value'] = df['Характеристики'].apply(my_function)
+df['ECLASS name'] = df['Характеристики'].apply(my_function)
 
-# Сохраняем измененный DataFrame в новый CSV файл
-df.to_csv('/home/sana451/PycharmProjects/scrapy_parsers/schmersal_com/schmersal_com/results/schmersal_com_.de.csv', index=False)
+# # Сохраняем измененный DataFrame в новый CSV файл
+df.to_csv(
+    '/home/sana451/PycharmProjects/scrapy_parsers/de_rubix_com/de_rubix_com/results/de_rubix_com.all.de.csv',
+    index=False
+)
 
 print("Файл успешно сохранен!")
