f67e4bad9cce5bbf0d340f03e5fde8f55d8a6ef5
Sana451 <sana451@mail.ru>
2025-05-29 15:50:19 +0300

    Read read_column_from_csv_file by pandas

--- scraping_tools/csv_tools.py
+++ scraping_tools/csv_tools.py
@@ -1,4 +1,19 @@
 import csv
+import pandas as pd
+
+def read_column_from_csv_file(path: str, column_name: str) -> list:
+    """
+    Читает указанный столбец по названию из CSV-файла.
+
+    :param path: Путь к CSV-файлу.
+    :param column_name: Название столбца, который нужно извлечь.
+    :return: Список значений из указанного столбца.
+    :raises ValueError: Если указанный столбец не найден.
+    """
+    df = pd.read_csv(path, encoding="utf-8")
+    if column_name not in df.columns:
+        raise ValueError(f"Столбец '{column_name}' не найден в файле.")
+    return df[column_name].tolist()
 
 
 def read_row_from_csv_file(path: str, num: int = 0) -> list:
